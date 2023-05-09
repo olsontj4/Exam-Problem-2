5e005dd3db6459bce2b49d2304dada232f59dfb6
olsontj4 <olsontj4@my.lanecc.edu>
2023-05-09 14:54:56 -0700

    Exam problem 2 complete.

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word or phrase:");
+            if (IsPalindrome(Console.ReadLine()))
+            {
+                Console.WriteLine("This is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("This is not a palindrome.");
+            }
+        }
+        static bool IsPalindrome(string input)
+        {
+            Queue<string> forwards = new Queue<string>();
+            Stack<string> reverse = new Stack<string>();
+            foreach (char c in input.ToLower())//Fill both data structures with only letters.
+            {
+                if (Char.IsLetter(c))
+                {
+                    forwards.Enqueue(c.ToString());
+                    reverse.Push(c.ToString());
+                }
+            }
+            for (int i = 0; i < forwards.Count; i++)//Empty data structures and check for mismatch.
+            {
+                if (forwards.Dequeue() != reverse.Pop())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
